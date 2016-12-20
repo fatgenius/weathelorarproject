1d79568c159840ee18be76a5063dab13c5c164dd
GermanNovikov <zilbermane@yahoo.de>
2016-12-20 14:31:59 +0100

    Reconnect added. Methods for parsing received message added. Javadoc added. TestJson class removed.

--- src/main/java/com/SaxionWeatherStation/App.java
+++ src/main/java/com/SaxionWeatherStation/App.java
@@ -5,36 +5,49 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.json.JSONObject;
 
 /**
- * Hello world!
- *
+ * This application uses eclipse paho client to receive data from broker.
+ * Furthermore its saves the received data to database.
+ * @see <a href="https://eclipse.org/paho/">https://eclipse.org/paho/</a>
  */
 public class App {
 
     MqttClient client;
     String topic        = "+/devices/+/up";
-    //int qos             = 0;
     String broker       = "tcp://staging.thethingsnetwork.org:1883";
     String clientId     = "saxion_station";
     MemoryPersistence persistence = new MemoryPersistence();
 
+
+    /**
+     * default constructor
+     */
     public App() {
     }
 
     public static void main(String[] args) {
-        new App().doDemo();
+        new App().receiveData();
     }
 
-    public void doDemo() {
+    /**
+     * Creates new client and manages the data received by the client from broker
+     */
+    public void receiveData() {
         try {
             client = new MqttClient(broker, clientId, persistence);
 
             client.setCallback(new MqttCallback() {
 
-                //@Override
-                public void connectionLost(Throwable cause) { //Called when the client lost the connection to the broker
+
+                public void connectionLost(Throwable cause) {//Called when the client lost the connection to the broker
+                    System.out.println("connection lost");
+                    try {
+                        connect();
+                    }
+                    catch (MqttException e) {
+                        e.printStackTrace();
+                    }
                 }
 
-                //@Override
                 public void messageArrived(String topic, MqttMessage message) throws Exception {
                     System.out.println("new" + message.toString());
                     JSONObject jsonObject = new JSONObject(message.toString());
@@ -43,7 +56,7 @@ public class App {
                         if(jsonObject2.has("message")){
                             String str = jsonObject2.getString("message");
                             System.out.println(str);
-                            dataToDatabase(str);
+                            parseMessage(str);
 
                         } else {
                             // It doesn't exist, do nothing
@@ -51,35 +64,116 @@ public class App {
                     } else {
                         // It doesn't exist, do nothing
                     }
-                    //check if needed field exists
-                    //get value of the field
                 }
 
-                //@Override
                 public void deliveryComplete(IMqttDeliveryToken token) {//Called when a outgoing publish is complete
                 }
             });
 
-            MqttConnectOptions connOpts = new MqttConnectOptions();
-            connOpts.setCleanSession(false);
-            connOpts.setUserName("70B3D57ED00018F6");
-            connOpts.setPassword("r7cAAHo0pY17udmgsvIP9HvL1mlmCbzh9kWbOQGKVLs=".toCharArray());
-            client.connect(connOpts);
-            client.subscribe(topic);
+            connect();
 
         } catch (MqttException e) {
             e.printStackTrace();
         }
     }
-    private void dataToDatabase(String str) {
-        String[] parts = str.split("/");
-        String part1 = parts[0];
-        String part2 = parts[1];
-        String part3 = parts[2];
-        String part4 = parts[3];
-        System.out.println(part1);
-        System.out.println(part2);
-        System.out.println(part3);
-        System.out.println(part4);
+
+    /**
+     * Sets up connection options, connects to broker and subscribes to topic
+     * @throws MqttException
+     */
+    private void connect() throws MqttException {
+        MqttConnectOptions connOpts = new MqttConnectOptions();
+        connOpts.setCleanSession(false);
+        connOpts.setUserName("70B3D57ED00018F6");
+        connOpts.setPassword("r7cAAHo0pY17udmgsvIP9HvL1mlmCbzh9kWbOQGKVLs=".toCharArray());
+        client.connect(connOpts);
+        System.out.println("connected to: " + broker);
+        client.subscribe(topic);
+        System.out.println("subscribed to: " + topic);
+    }
+
+    /**
+     * parses data from the received message. Futhermore calls method to insert parsed data into database
+     * @param str strings that contains received message
+     */
+    private void parseMessage(String str) {
+        String[] parameters = str.split("/");
+        int numberOfParameters = 4;
+
+        if ( isParsable(parameters, numberOfParameters) ) {
+            int x = Integer.parseInt(parameters[0]);
+            int w = Integer.parseInt(parameters[1]);
+            int z = Integer.parseInt(parameters[2]);
+            float f = Float.parseFloat(parameters[3]);
+            System.out.println(x);
+            System.out.println(w);
+            System.out.println(z);
+            System.out.println(f);
+            //futhermore insert into database
+        }
+    }
+
+    /**
+     * checks if the received message is in proper format
+     * @param parameters array of parameters extracted from recived message (temp, humidity ...)
+     * @param numberOfParameters expected number of extracted parameters
+     * @return return true if the recived message is in valid format and it can parsed and added to database
+     */
+    private boolean isParsable(String [] parameters, int numberOfParameters) {
+        if (parameters.length == numberOfParameters) {
+
+            if (    isInteger(parameters[0]) &&
+                    isInteger(parameters[1]) &&
+                    isInteger(parameters[2]) &&
+                    isFloat(parameters[3])  )
+            {
+                return true;
+            }
+            else {
+                System.out.println("invalid data format");
+                return false;
+            }
+        }
+
+        else {
+            System.out.println("invalid data format");
+            return false;
+        }
+    }
+
+    /**
+     * Checks if string can be parsed to integer number
+     * @param str string to be checked
+     * @return returns true if string can be parsed to integer
+     */
+    private static boolean isInteger(String str)
+    {
+        try
+        {
+            int i = Integer.parseInt(str);
+        }
+        catch(NumberFormatException nfe)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Checks if string can be parsed to float number
+     * @param str string to be checked
+     * @return returns true if string can be parsed to integer
+     */
+    private static boolean isFloat(String str)
+    {
+        try
+        {
+            float f = Float.parseFloat(str);
+        }
+        catch(NumberFormatException nfe)
+        {
+            return false;
+        }
+        return true;
     }
 }

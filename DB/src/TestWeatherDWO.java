7bbb621ede9095d17065b37bce6b60e29903eb22
DaveOuds <daveouds@gmail.com>
2017-01-12 11:45:03 +0100

    Add files via upload
    Database

--- DB/src/TestWeatherDWO.java
+++ DB/src/TestWeatherDWO.java
@@ -2,7 +2,10 @@
  * Created by Dave Ouds& Rene Potgieter on 28-10-2016.
  */
 
+import sun.java2d.pipe.SpanShapeRenderer;
+
 import java.sql.*;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 
@@ -10,6 +13,7 @@ public class TestWeatherDWO {
     public static void main(String[] args) {
         DBmanager dbcon = null;
         Connection conn = null;
+
         try {
             //make a connection with the database
             dbcon = DBmanager.getInstance();
@@ -17,8 +21,11 @@ public class TestWeatherDWO {
 
             Statement stmt = conn.createStatement();
             Calendar cal = Calendar.getInstance();
+            SimpleDateFormat sdft = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+            String time = sdft.format(cal.getTime());
 
-            ResultSet rs = stmt.executeQuery("SELECT country.name FROM country;"); // SQL statement to push the data in the database.
+
+            ResultSet rs = stmt.executeQuery("insert into Datas (Times,Temperature, Air_Pressure,Humidity,Windspeed,Brightness) values ('" +time +"','"+temperature +"','" +pressure +"','" +humidity +"','" +windspeed +"','"+brightness +"');"); // SQL statement to push the data in the database.
 
             //make a Statement object
             // execute the sqlstatement the result is resultset
